69f3391b03ea3f41431fe3ba5143eccdca27603c
Gabi Marrreto <marretogabriella@gmail.com>
2023-05-26 23:10:14 -0300

    finalização do painel carona

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,13 +1,12 @@
 import { StatusBar, SafeAreaView, StyleSheet, Text } from 'react-native';
 import Footer from "./Comps/Footer"
 import React from 'react';
-import PainelCarona from '../src/PainelCarona';
+import PainelCarona from './src/PainelCarona';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <PainelCarona/>
-      <Text>Pra por na tela tem q ta dentro to text</Text>
       <StatusBar style="auto" />
       <Footer />
     </SafeAreaView>
