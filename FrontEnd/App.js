05a9db0560fefe2fb7fb8373539daea30c67db3a
julio.so <julio.so@codengage.com>
2023-05-30 03:49:25 -0300

    adiciona telas

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Footer from "./Comps/Footer"
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Footer from "./Comps/Footer";
+import LoginScreen from "./src/Login/Login";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen></LoginScreen>
       <StatusBar style="auto" />
-      <Footer />
     </View>
   );
 }
@@ -15,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ FrontEnd/src/Login/Login.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+
+const LoginScreen = () => {
+  const [ra, setRa] = useState("");
+  const [password, setPassword] = useState("");
+  const logo = require("../../icons/Logo.png");
+
+  const handleLogin = () => {
+    console.log("RA:", ra);
+    console.log("Password:", password);
+  };
+  const handleRegister = () => {
+    console.log("send to register page");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <Text style={[styles.title, styles.textShadow]}>Login</Text>
+      <TextInput
+        style={[styles.input, styles.shadow]}
+        placeholder="RA do aluno"
+        value={ra}
+        keyboardType="numeric"
+        onChangeText={(ra) => setRa(ra.replace(/[^0-9]/g, ""))}
+      />
+      <TextInput
+        style={[styles.input, styles.shadow]}
+        placeholder="Senha"
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={[styles.button, styles.shadow]}
+        onPress={handleLogin}
+      >
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleRegister}>
+        <Text style={styles.textShadow}>Não possui uma conta? Crie agora!</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 225,
+    height: 225,
+    marginBottom: 0,
+  },
+  title: {
+    fontSize: 36,
+    marginBottom: 40,
+  },
+  input: {
+    color: "black",
+    backgroundColor: "#D9D9D9",
+    borderRadius: 30,
+    paddingVertical: 15,
+    paddingHorizontal: 35,
+    width: 275,
+    marginVertical: 10,
+    fontSize: 14,
+  },
+  button: {
+    backgroundColor: "#FF2B2B",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    paddingVertical: 15,
+    paddingHorizontal: 35,
+    width: 159,
+    marginTop: 50,
+    marginBottom: 90,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+  },
+  shadow: {
+    shadowColor: "black",
+    elevation: 5,
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  textShadow: {
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 8,
+  },
+});
+
+export default LoginScreen;
